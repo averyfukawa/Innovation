8e4fa6c0e582235866dfa3f8ac65c6d3b2455def
averyfukawa <averyfukawa@users.noreply.github.com>
2019-11-27 20:59:49 +0100

    Scripts & FMOD builds

---
+++ Assets/Scripts/Audio Scripts/BKM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Audio_Scripts
+{
+    public class BKM : MonoBehaviour
+    {
+        [SerializeField] private int currentMusic;
+
+        private FMOD.Studio.EventInstance _music;
+
+        private void Start()
+        {
+            _music = FMODUnity.RuntimeManager.CreateInstance("event:/BKM");
+            _music.start();
+        }
+
+        private void Update()
+        {
+            _music.setParameterByName("song", currentMusic);
+        }
+    }
+}
---
+++ Assets/Scripts/Audio Scripts/SFX.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Audio_Scripts
+{
+    public class SFX : MonoBehaviour
+    {
+        [FMODUnity.EventRef] public string sfxEvent;
+
+        private bool _isPlaying = false;
+
+        [SerializeField] private float clipLength;
+
+        private void Start()
+        {
+            if (sfxEvent == null)
+                throw new Exception("SFX Event not set in " + gameObject.name);
+        }
+
+        public void Play(string fmodEvent)
+        {
+            if (_isPlaying == false)
+            {
+                sfxEvent = "event:/SFX/" + fmodEvent;
+                
+                FMODUnity.RuntimeManager.PlayOneShot(sfxEvent, GetComponent<Transform>().position);
+                
+                _isPlaying = true;
+                
+                StartCoroutine(WaitForEnd(clipLength));
+            }
+        }
+
+        private IEnumerator WaitForEnd(float length)
+        {
+            if (length <= 0) throw new ArgumentOutOfRangeException("Length of clip is not set in " + " " + gameObject.name + nameof(length));
+            
+            length = clipLength;
+            yield return new WaitForSeconds(length);
+            _isPlaying = false;
+        }
+    
+    }
+}
