97bb063ceff6842f8e3cfc2375ddda9e6ec4eba3
averyfukawa <averyfukawa@users.noreply.github.com>
2019-11-30 01:10:08 +0100

    Main menu audio works, ambiance works, BKM works and doesn't get destroyed during scene changes

--- Assets/Scripts/UI3DClickable.cs
+++ Assets/Scripts/UI3DClickable.cs
@@ -1,13 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Audio_Scripts;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class UI3DClickable : MonoBehaviour
 {
     [SerializeField] string switchToScene;
-    [SerializeField] string audioFile; 
 
+    [SerializeField] private SFX sounds;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,10 @@ public class UI3DClickable : MonoBehaviour
         if(switchToScene != null)
         {
             DontDestroyOnLoad(this);
+            
+            Debug.Log("The Spatial UI was clicked!");
+            sounds.Play("SFX/UI Click");
+            
             SceneManager.LoadScene(switchToScene);
         }
     }
--- Assets/Scripts/UI3DHandler.cs
+++ Assets/Scripts/UI3DHandler.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Audio_Scripts;
 using UnityEngine;
 
 public class UI3DHandler : MonoBehaviour
@@ -7,6 +8,9 @@ public class UI3DHandler : MonoBehaviour
     [SerializeField] GameObject controller;
     Vector3 startOrientation;
 
+    [SerializeField] private SFX sounds;
+
+    private bool hasHovered = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,21 @@ public class UI3DHandler : MonoBehaviour
         Vector3 rayDirection = controller.transform.forward;
 
         Debug.DrawRay(controller.transform.position, rayDirection, Color.red);
+        if (Physics.Raycast(controller.transform.position, rayDirection, out hit))
+        {
+            UI3DClickable clickable;
+            if (hit.transform.TryGetComponent<UI3DClickable>(out clickable))
+            {
+                if (hasHovered == false)
+                {
+                    Debug.Log("The Spatial UI was hovered!");
+                    sounds.Play("SFX/UI Hover");
+                    hasHovered = true;
+                }
+            }
+        }
+        else 
+            hasHovered = false;
 
         if(Input.GetMouseButtonDown(0) && Physics.Raycast(controller.transform.position, rayDirection, out hit))
         {
