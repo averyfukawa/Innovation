c02a3771f06ce5628df946cd6dcb236150d28467
averyfukawa <averyfukawa@users.noreply.github.com>
2019-12-01 03:22:09 +0100

    Chippy Kody conversation event set up, script set up

---
+++ Assets/Scripts/Audio Scripts/ChippyKodyConvo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Audio_Scripts;
+using UnityEngine;
+
+public class ChippyKodyConvo : MonoBehaviour
+{
+    [SerializeField] private SFX conversationSounds;
+    [SerializeField] private float delayStart;
+    
+    void Start()
+    {
+        if (conversationSounds == null)
+            throw new Exception("Chippy Kody conversation is empty at " + gameObject.name);
+        
+        StartCoroutine(Convo(delayStart));
+    }
+
+    private IEnumerator Convo(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        conversationSounds.Play("Conversations/mission");
+    }
+}
