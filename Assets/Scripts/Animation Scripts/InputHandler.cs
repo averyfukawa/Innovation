e7bba86ca84b7a534c26162e7361be7f47d8c9b6
iVendrix <58091030+iVendrix@users.noreply.github.com>
2019-12-02 01:53:27 +0100

    Added animations

---
+++ Assets/Scripts/Animation Scripts/InputHandler.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InputHandler : MonoBehaviour
+{
+    [SerializeField] GameObject controller;
+
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 rayDirection = controller.transform.forward;
+        if (Physics.Raycast(controller.transform.position, rayDirection, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                ToyCar toycar = hit.transform.GetComponent<ToyCar>();
+                if (toycar)
+                    toycar.PlayToyCarAnim();
+            }
+        }
+    }
+}
